5add0c73ba6c1c14236fa0830b938e99c0c3ecd0
Ryan <shameem@seemymachines.com>
2025-12-02 16:54:37 +0530

    functionalities completed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,37 @@
+import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 import Header from "../Header";
 import Employeelist from "../EmployeeList";
 import styles from "./index.module.css";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      navigate("/users/login");
+      return;
+    }
+
+    try {
+      const decoded = jwtDecode(token);
+      console.log("Expires at:", new Date(decoded.exp * 1000));
+
+      const isExpired = decoded.exp < Math.floor(Date.now() / 1000);
+
+      if (isExpired) {
+        localStorage.removeItem("token");
+        navigate("/users/login"); 
+      }
+    } catch (error) {
+      localStorage.removeItem("token");
+      navigate("/users/login"); 
+    }
+  }, [navigate]);
+
   return (
     <div className={styles.homeContainer}>
       <Header />
